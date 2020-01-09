41598276c8533633beaa5d6a0307d328cd384a97
MarsiBarsi <darragon-nn@yandex.ru>
2020-01-09 19:06:45 +0300

    declare global context in Safari

--- projects/ng-web-audio/src/polyfill.js
+++ projects/ng-web-audio/src/polyfill.js
@@ -1,4 +1,4 @@
-AudioContext = AudioContext || webkitAudioContext;
+AudioContext = globalThis.AudioContext || globalThis.webkitAudioContext;
 
 const context = new AudioContext();
 
@@ -6,39 +6,39 @@ try {
     const gain = new GainNode(context);
 } catch (e) {
     // Constructor for AudioNode not supported, probably running Safari
-    function BiquadFilterNode(context) {
+    globalThis.BiquadFilterNode = function(context) {
         return context.createBiquadFilter();
-    }
-    function ConvolverNode(context) {
+    };
+    globalThis.ConvolverNode = function(context) {
         return context.createConvolver();
-    }
-    function DelayNode(context) {
+    };
+    globalThis.DelayNode = function(context) {
         return context.createDelay();
-    }
-    function DynamicsCompressorNode(context) {
+    };
+    globalThis.DynamicsCompressorNode = function(context) {
         return context.createDynamicsCompressor();
-    }
-    function GainNode(context) {
+    };
+    globalThis.GainNode = function(context) {
         return context.createGain();
-    }
-    function PannerNode(context) {
+    };
+    globalThis.PannerNode = function(context) {
         return context.createPanner();
-    }
-    function StereoPannerNode(context) {
+    };
+    globalThis.StereoPannerNode = function(context) {
         return context.createStereoPanner();
-    }
-    function WaveShaperNode(context) {
+    };
+    globalThis.WaveShaperNode = function(context) {
         return context.createWaveShaper();
-    }
-    function AudioBufferSourceNode(context) {
+    };
+    globalThis.AudioBufferSourceNode = function(context) {
         return context.createBufferSource();
-    }
-    function MediaElementAudioSourceNode(context, element) {
+    };
+    globalThis.MediaElementAudioSourceNode = function(context, element) {
         return context.createMediaElementSource(element);
-    }
-    function OscillatorNode(context) {
+    };
+    globalThis.OscillatorNode = function(context) {
         return context.createOscillator();
-    }
+    };
 }
 
 context.close();
