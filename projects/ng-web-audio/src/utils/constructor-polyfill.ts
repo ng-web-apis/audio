264c87272d32b28c54049f418181f4719d6173a2
waterplea <alexander@inkin.ru>
2020-01-14 18:11:25 +0300

    test commit

--- projects/ng-web-audio/src/utils/constructor-polyfill.ts
+++ projects/ng-web-audio/src/utils/constructor-polyfill.ts
@@ -4,10 +4,33 @@ export function constructorPolyfill<T extends AudioNode>(that: any, node: T) {
     }
 }
 
-function polyfillAudioNode(that: any, node: AudioNode) {
+function polyfillAudioNode<T extends AudioNode>(that: any, node: T) {
     const proto = Object.getPrototypeOf(node);
+    const props = Object.getOwnPropertyNames(proto);
     const audioNodeProto = Object.getPrototypeOf(proto);
+    const audioNodeProps = Object.getOwnPropertyNames(audioNodeProto);
 
-    Object.defineProperties(that, Object.getOwnPropertyDescriptors(audioNodeProto));
-    Object.defineProperties(that, Object.getOwnPropertyDescriptors(proto));
+    [...audioNodeProps, ...props].forEach(prop => {
+        const descriptor =
+            Object.getOwnPropertyDescriptor(proto, prop) ||
+            Object.getOwnPropertyDescriptor(audioNodeProto, prop);
+
+        if (prop !== 'constructor' && descriptor) {
+            const {writable, enumerable, configurable} = descriptor;
+            let {get, set, value} = descriptor;
+
+            get = get ? get.bind(node) : undefined;
+            set = set ? set.bind(node) : undefined;
+            value = typeof value === 'function' ? value.bind(node) : value;
+
+            Object.defineProperty(that, prop, {
+                writable,
+                enumerable,
+                configurable,
+                get,
+                set,
+                value,
+            });
+        }
+    });
 }
