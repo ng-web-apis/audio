9b869a2f52d16247b06b87ca42cf9220431f3509
waterplea <alexander@inkin.ru>
2020-01-23 11:53:41 +0300

    fix test

--- projects/audio/src/pipes/tests/periodic-wave.pipe.spec.ts
+++ projects/audio/src/pipes/tests/periodic-wave.pipe.spec.ts
@@ -1,36 +1,9 @@
-import {Component, ViewChild} from '@angular/core';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {WebAudioModule} from '../../module';
 import {WebAudioPeriodicWavePipe} from '../periodic-wave.pipe';
 
 describe('waPeriodicWave', () => {
-    @Component({
-        template: `
-            <div waOscillatorNode [periodicWave]="[10] | waPeriodicWave"></div>
-        `,
-    })
-    class TestComponent {
-        @ViewChild(WebAudioPeriodicWavePipe)
-        pipe!: WebAudioPeriodicWavePipe;
-    }
-
-    let fixture: ComponentFixture<TestComponent>;
-    let testComponent: TestComponent;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [WebAudioModule],
-            declarations: [TestComponent],
-        });
-    });
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(TestComponent);
-        testComponent = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    const pipe = new WebAudioPeriodicWavePipe(new AudioContext());
 
     it('creates PeriodicWave', () => {
-        expect(testComponent.pipe.transform([10]) instanceof PeriodicWave).toBe(true);
+        expect(pipe.transform([10]) instanceof PeriodicWave).toBe(true);
     });
 });
